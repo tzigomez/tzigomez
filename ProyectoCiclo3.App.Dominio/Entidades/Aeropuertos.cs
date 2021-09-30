9f8f41943480d8606b4f8f0451aef1b66cb13b35
tzigomez <tzitzitlinigpe10@gmail.com>
2021-09-30 11:44:25 -0500

    First commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aeropuertos{
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+        public int coord_x { get; set; }
+        public int coord_y { get; set; }
+    }
+ 
+}
---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20210920053534_MigraInicial.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class MigraInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aeropuertos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ciudad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pais = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    coord_x = table.Column<int>(type: "int", nullable: false),
+                    coord_y = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aeropuertos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aviones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modelo = table.Column<int>(type: "int", nullable: false),
+                    numero_asientos = table.Column<int>(type: "int", nullable: false),
+                    numero_banos = table.Column<int>(type: "int", nullable: false),
+                    capacidad_maxima = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aviones", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rutas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    origen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    destino = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    tiempo_estimado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rutas", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aeropuertos");
+
+            migrationBuilder.DropTable(
+                name: "Aviones");
+
+            migrationBuilder.DropTable(
+                name: "Rutas");
+        }
+    }
+}
